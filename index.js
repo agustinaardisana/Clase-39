f7e2057f3e200b8cd2e8fa4900caafb535651695
agustinaardisana <agustinaardisana@gmail.com>
2020-10-19 19:22:44 -0500

    agrego ejercicios 5 y 6

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-// //Ejercicio 1
-const bandas = ["Ramones","Maluma","Ozuna","Babasonicos","Bruno Mars"]
+// // //Ejercicio 1
+// const bandas = ["Ramones","Maluma","Ozuna","Babasonicos","Bruno Mars"]
 
-const primeraBanda = bandas[0]
-const terceraBanda = bandas[2]
-const quintaBanda = bandas[4]
+// const primeraBanda = bandas[0]
+// const terceraBanda = bandas[2]
+// const quintaBanda = bandas[4]
 
-console.log(`La banda en primer lugar es: ${primeraBanda}. La banda en tercer lugar es: ${terceraBanda}.
-La banda en sexto lugar es: ${quintaBanda}`)
+// console.log(`La banda en primer lugar es: ${primeraBanda}. La banda en tercer lugar es: ${terceraBanda}.
+// La banda en sexto lugar es: ${quintaBanda}`)
 
-//Ejercicio 2
-const valorNotas = [1,2,3,4,5,6,7,8,9,10]
+// //Ejercicio 2
+// const valorNotas = [1,2,3,4,5,6,7,8,9,10]
 
-const sumarNotas = () => {
+// const sumarNotas = () => {
 
-    let suma = 0
-    for (let i = 0; i < valorNotas.length; i++) {
-        suma = suma + valorNotas[i]
-    }
-    return suma
-}
+//     let suma = 0
+//     for (let i = 0; i < valorNotas.length; i++) {
+//         suma = suma + valorNotas[i]
+//     }
+//     return suma
+// }
 
-console.log(sumarNotas())
-console.log(valorNotas.length)
-
-
-const calcularPromedio = () => {
-
-    let promedio = 0
-    promedio = sumarNotas() / valorNotas.length
-    return promedio
-}
-
-console.log(calcularPromedio())
+// console.log(sumarNotas())
+// console.log(valorNotas.length)
 
 
-//Ejercicio 3
+// const calcularPromedio = () => {
 
-const playlist = ["Concrete and Gold", "The Line", "Sunday Rain", "Happy Ever After (Zero Hour)", "Arrows", "Dirty Water", "La Dee Da", "The Sky Is a Neighborhood", "Make It Right", "Run", "T-Shirt"];
+//     let promedio = 0
+//     promedio = sumarNotas() / valorNotas.length
+//     return promedio
+// }
 
-for (let i=0; i < playlist.length; i++) {
-    console.log(playlist[i])
-}
+// console.log(calcularPromedio())
 
-for (let i = playlist.length-1; i >= 0; i--) {
-    console.log(playlist[i])
-}
+
+// //Ejercicio 3
+
+// const playlist = ["Concrete and Gold", "The Line", "Sunday Rain", "Happy Ever After (Zero Hour)", "Arrows", "Dirty Water", "La Dee Da", "The Sky Is a Neighborhood", "Make It Right", "Run", "T-Shirt"];
+
+// for (let i=0; i < playlist.length; i++) {
+//     console.log(playlist[i])
+// }
+
+// for (let i = playlist.length-1; i >= 0; i--) {
+//     console.log(playlist[i])
+// }
 
 //Ejercicio 4
 const items = ['🍉', '🍐', '🍌', '🍇', '🍎', '🍊']
@@ -53,10 +53,32 @@ const obtenerNumeroAlAzar = () => {
     return Math.floor(Math.random(0,1)*5)
 }
 
-console.log(obtenerNumeroAlAzar())
+//console.log(obtenerNumeroAlAzar())
 
 const obtenerFrutaAlAzar = () => {
 
     return items[obtenerNumeroAlAzar()]
 }
-console.log(obtenerFrutaAlAzar())
+//console.log(obtenerFrutaAlAzar())
+
+//Ejercicio 5
+const listaDeFrutas = []
+
+listaDeFrutas[0] = obtenerFrutaAlAzar()
+listaDeFrutas[1] = obtenerFrutaAlAzar()
+listaDeFrutas[2] = obtenerFrutaAlAzar()
+listaDeFrutas[3] = obtenerFrutaAlAzar()
+listaDeFrutas[4] = obtenerFrutaAlAzar()
+
+console.log(listaDeFrutas)
+
+//Ejercicio 6
+let otraListaDeFrutas = []
+
+for (let i = 0; i < 5; i++) {
+    otraListaDeFrutas[i] = obtenerFrutaAlAzar()
+}
+
+console.log(otraListaDeFrutas)
+
+
